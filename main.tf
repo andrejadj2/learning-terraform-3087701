a578b9187b5a304ca1866d176346d9568c139b04
andrejadj2 <146332528+andrejadj2@users.noreply.github.com>
2025-06-27 12:59:53 +0200

    Changed version of security group module

--- main.tf
+++ main.tf
@@ -44,9 +44,9 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-  name    = "blog"
-
+  version = "4.13.0"
+  
+  name   = "blog"
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
