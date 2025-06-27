aa4752b0f19bd54d8902bc7fe42b4d7ea90ff4da
andrejadj2 <146332528+andrejadj2@users.noreply.github.com>
2025-06-27 17:28:37 +0200

    added versions.tf

--- main.tf
+++ main.tf
@@ -33,14 +33,14 @@ module "vpc" {
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.0"
+  version = "6.5.2"
 
   name = "blog"
   min_size = 1
   max_size = 2
 
   vpc_zone_identifier = module.vpc.public_subnets
-  alb_target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
