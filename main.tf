9dcf4beed75169649d8fc0b9d582175bc98a510f
andrejadj2 <146332528+andrejadj2@users.noreply.github.com>
2025-06-27 18:23:58 +0200

    updated main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "blog_vpc" {
 
 resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = module.blog_autoscaling.autoscaling_group_name
-  lb_target_group_arn    = module.blog_alb.target_group_arns[0]
+  lb_target_group_arn    = module.blog_alb.target_groups["blog-0"].arn
 }
 
 module "blog_autoscaling" {
