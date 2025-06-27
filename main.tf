2b4cb15fed14573331f7a3874f7a94462a621cdf
andrejadj2 <146332528+andrejadj2@users.noreply.github.com>
2025-06-27 13:07:09 +0200

    Changed enable_nat_gateway to false

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = false
 
   tags = {
     Terraform = "true"
@@ -44,9 +44,9 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
-  
-  name   = "blog"
+  version = "5.3.0"
+  name    = "blog"
+
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
