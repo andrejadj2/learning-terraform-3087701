94998046a343916dfac430368d215d10e1700008
andrejadj2 <146332528+andrejadj2@users.noreply.github.com>
2025-07-01 11:51:41 +0200

    04_02

--- main.tf
+++ main.tf
@@ -29,15 +29,17 @@ module "blog_vpc" {
   }
 }
 
+
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.0"
+  version = "6.5.2"
 
   name = "blog"
 
-  min_size            = 0
-  max_size            = 0
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
@@ -45,7 +47,7 @@ module "blog_autoscaling" {
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "9.17.0"
+  version = "~> 6.0"
 
   name = "blog-alb"
 
@@ -61,25 +63,25 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      create_attachment = false # ← ti ručno praviš attachment
     }
   ]
 
-  listeners = []
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
     Environment = "dev"
   }
 }
 
-resource "aws_autoscaling_attachment" "asg_attachment" {
-  autoscaling_group_name = module.blog_autoscaling.autoscaling_group_name
-  lb_target_group_arn    = values(module.blog_alb.target_groups)[0].arn
-}
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
+  version = "4.13.0"
 
   vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
