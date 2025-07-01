d3fab283538b1461e4d50be1610577e9fda7fef7
andrejadj2 <146332528+andrejadj2@users.noreply.github.com>
2025-07-01 13:18:51 +0200

    added target groups through launch template

--- main.tf
+++ main.tf
@@ -40,10 +40,13 @@ module "blog_autoscaling" {
   min_size            = 1
   max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = [module.blog_alb.target_groups["blog_tg"].arn]
   security_groups     = [module.blog_sg.security_group_id]
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
+
+  # Dodajte target grupe kroz launch template
+  launch_template_name = "blog-lt"
+  target_group_arns    = [module.blog_alb.target_groups["blog_tg"].arn]
 }
 
 module "blog_alb" {
