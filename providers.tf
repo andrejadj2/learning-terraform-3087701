8faf601694ed5de69438df0cdabd6afb5563b34f
andrejadj2 <146332528+andrejadj2@users.noreply.github.com>
2025-07-01 12:28:51 +0200

    changed versions

--- providers.tf
+++ providers.tf
@@ -2,8 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      # fiksiraj se na 6.0.x seriju, isključi 6.1.0:
-      version = ">= 6.0.0, < 6.1.0"
+      version = "= 6.0.0
     }
   }
 }
