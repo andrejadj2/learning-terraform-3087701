7fcf45f68112c09a5644bcdd1e8cc841d5e5fa64
andrejadj2 <146332528+andrejadj2@users.noreply.github.com>
2025-07-01 13:01:36 +0200

    new structure for listeners and target groups

--- main.tf
+++ main.tf
@@ -58,22 +58,26 @@ module "blog_alb" {
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
-  target_groups = [
-    {
+  # Promenjeno: Nova struktura za listenere
+  listeners = {
+    http = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_key = "blog_tg"
+      }
+    }
+  }
+
+  # Promenjeno: Nova struktura za target groups
+  target_groups = {
+    blog_tg = {
       name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
     }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
+  }
 
   tags = {
     Environment = "dev"
